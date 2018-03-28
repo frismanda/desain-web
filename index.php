21ab29918a0e11cf2addc61eeb8628ca601a8a53
KADAL\KADAL <frismanda@gmail.com>
2018-03-28 09:39:08 +0700

    Next commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VoteMe!</title>
+	<!-- JS SOURCE -->
+	<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="swal/sweetalert2.all.js"></script>
+
+	<!-- CSS SOURCE -->
+	<link rel="stylesheet" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.css.map">
+	<link rel="stylesheet" href="css/bootstrap-reboot.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+	  <div class="wrapper">
+	    <form class="form-signin">       
+	      <h2 class="form-signin-heading">Masuk Dong</h2>
+	      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+	      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+	      
+	      <button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>   
+	    </form>
+	  </div>
+
+</body>
+</html>
