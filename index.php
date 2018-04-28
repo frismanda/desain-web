c03d64cd43e8b1f8703b57af0d018bafefb3ca29
KADAL\KADAL <frismanda@gmail.com>
2018-04-28 23:41:03 +0700

    baru baru beranda

--- index.php
+++ index.php
@@ -15,15 +15,31 @@
 	<link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
+
 	  <div class="wrapper">
 	    <form class="form-signin">       
 	      <h2 class="form-signin-heading">Masuk Dong</h2>
-	      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-	      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+	      <input type="text" class="form-control" id="username" name="username" placeholder="Email Address" required="" autofocus="" />
+	      <input type="password" class="form-control" id="password" name="password" placeholder="Password" required=""/>      
 	      
-	      <button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>   
+	      <button class="btn btn-lg btn-primary btn-block" type="button" id="masukdong">Masuk</button>   
 	    </form>
 	  </div>
 
 </body>
+<script type="text/javascript">
+	$(document).ready(function(){
+
+		$('#masukdong').click(function(){
+			if ($("#username").val() == "frismanda" && $("#password").val() == "mandaganteng" ) {
+			swal('Sukses', '-', 'success');
+			window.location.href = "beranda.html";
+		} else {
+			swal('Nonono', 'Ohh tidak bisa nih', 'warning');
+		}
+
+		})
+		
+	})
+</script>
 </html>
